2ffea605ad4e986db624bf08f67513ed2991a596
ak7396 <murarishettysaiakhil@gmail.com>
2026-02-25 20:21:35 -0600

    Json assig updated code feb 25

--- json-assignment.py
+++ json-assignment.py
@@ -1,4 +1,9 @@
-'''Task 2 — Parse an API Response
+'''Task 1 — Build a JSON Structure
+Create a Python dictionary representing a user profile with the following 
+fields: name, age, email, is_active, and a list of skills. 
+Convert it to a JSON string using json.dumps() and print it with proper indentation.
+
+Task 2 — Parse an API Response
 You receive the following mock API response as a JSON string:
 
 {"status": "success", "data": {"user_id": 101, "username": "alex99", "score": 87.5}}
@@ -21,7 +26,7 @@ Given the nested JSON below, extract and print the city and zip code of the user
 Then add a new key "country": "India" inside the address and print the updated JSON.
 
 '''
-#****************************** task 3 ******************************
+#****************************** task 1 ******************************
 
 import json #importing json module
 user_profile={"name":"sai", #its a python dictinoary 
@@ -32,7 +37,7 @@ user_profile={"name":"sai", #its a python dictinoary
 
 }
 
-json_format= json.dumps(ser_profile,indent=2) 
+json_format= json.dumps(user_profile,indent=2) 
 #json.dumps will convert py dict to json format and storing the result in a variable and print it
 print(json_format)
 
@@ -54,9 +59,9 @@ nested_json='''{
   }
 }'''
 
-dict_format=json.loads(nested_json)
-print(f'{dict_format["address"]["city"]} and {dict_format["address"]["zip"]}') #printed state adn zip code
-dict_format["address"]["Country"]="India"# i have added a new key valeu pair insisde a diction whic h is address 
-updated_json=json.dumps(dict_format,indent=2) #converting dict into json file as requested and printing it 
+dicts_format=json.loads(nested_json)
+print(f'{dicts_format["address"]["city"]} and {dicts_format["address"]["zip"]}') #printed state adn zip code
+dicts_format["address"]["Country"]="India"# i have added a new key valeu pair insisde a diction whic h is address 
+updated_json=json.dumps(dicts_format,indent=2) #converting dict into json file as requested and printing it 
 print(updated_json)
 
